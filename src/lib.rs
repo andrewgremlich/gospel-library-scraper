5535710edee90b12596826695ac869e12a4a16f7
andrewgremlich <andrew.gremlich@gmail.com>
2020-06-04 11:03:52 -0600

    Developed URL metadata parser.

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,35 @@ use scraper::Html;
 
 const CHURCH_ROOT: &str = "https://www.churchofjesuschrist.org";
 
-pub fn no_query_params(url: &str) -> &str {
-  let string_collection: Vec<&str> = url.split("?").collect();
-  string_collection[0]
+#[derive(Debug)]
+pub struct UrlReference {
+  file: String,
+  dir: String,
+  no_params: String,
+}
+
+pub fn urls_of_first_chapter(url: &str) -> UrlReference {
+  let no_query_param: Vec<&str> = url.split("?").collect();
+
+  let mut file_name_vector: Vec<&str> = no_query_param[0].split("/").collect();
+  file_name_vector.pop();
+  let dir_append: String = file_name_vector.join("/");
+
+  let mut file_to_write: String = ".".to_owned();
+  let mut dir_to_write: String = ".".to_owned();
+  
+  file_to_write.push_str(no_query_param[0]);
+  file_to_write.push_str(".md");
+
+  dir_to_write.push_str(&dir_append);
+
+  let urls = UrlReference {
+    file: String::from(file_to_write),
+    dir: String::from(dir_to_write),
+    no_params: String::from(no_query_param[0]),
+  };
+
+  return urls;
 }
 
 pub async fn navigate(url: &str) -> Result<Html, Box<dyn std::error::Error>> {
--- src/navigate_scripture_book/copier.rs
+++ src/navigate_scripture_book/copier.rs
@@ -1,7 +1,5 @@
-use gospellibraryscraper::{navigate, write_file};
+use gospellibraryscraper::{navigate, urls_of_first_chapter, write_file, UrlReference};
 use scraper::{Html, Selector};
-
-use gospellibraryscraper::no_query_params;
 
 async fn get_page() {
   println!("Get page");
@@ -25,10 +23,10 @@ fn parse_first_chapters(section_of_book: &Html, active_link: &Selector) {
     match element.value().attr("href") {
       Some(url) => {
         //get page code
-        let straight_url: &str = no_query_params(url);
+        let urls_of_first_chapter: UrlReference = urls_of_first_chapter(url);
+        println!("{:?}", urls_of_first_chapter);
         // get_page().await;
         order_number = order_number + 1;
-        println!("{:?}", straight_url);
       }
       _ => {
         // println!("No URL!");
@@ -41,8 +39,8 @@ pub async fn main(url: &str, index: u8) {
   let section_of_book: Html = navigate(url).await.unwrap();
 
   let active_link: Selector = Selector::parse("a.active-mDRbE").unwrap();
-  let list_with_book: Selector = Selector::parse("ul.active-mDRbE a.item-3cCP7").unwrap();
+  // let list_with_book: Selector = Selector::parse("ul.active-mDRbE a.item-3cCP7").unwrap();
 
   parse_first_chapters(&section_of_book, &active_link);
-  parse_books_with_chapters(&section_of_book, &list_with_book);
+  // parse_books_with_chapters(&section_of_book, &list_with_book);
 }
